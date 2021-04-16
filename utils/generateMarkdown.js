54b9520af2b3378251f0a00a619bc60006bf0c1b
Albert Jiang <albertjly@gmail.com>
2021-04-15 17:21:50 -0700

    generate a new readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -91,6 +91,7 @@ ${data.features}\n
 
 >## **_Contributing_**\n
 If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.\n
+❤️\n
 ${data.contribution}\n
 
 >## **_License_**\n
